93d48fc21c37cf348be5fac1d051d3e4412d0ca1
Jag <kwk4137@autuni.ac.nz>
2025-05-07 12:29:19 +1200

    Planning and creating packages with classes

---
+++ src/com/Inventory/model/ProductDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Inventory.model;
+
+/**
+ *
+ * @author jagrithnarayan
+ */
+// DAO stands for Data Access Object. It’s a design pattern used to separate the code that interacts with the 
+//database from the rest of the application logic (like your InventoryManager or GUI classes).
+
+public class ProductDAO {
+    
+}
