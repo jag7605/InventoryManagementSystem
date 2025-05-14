8742a9373260a1408a5738f119f39eb2e270923b
BonecaAmbalabu <yqz2107@autuni.ac.nz>
2025-05-14 13:40:39 +1200

    viewInventory and updateProduct Panel classes

--- src/com/Inventory/gui/UpdateProductPanel.java
+++ src/com/Inventory/gui/UpdateProductPanel.java
@@ -3,11 +3,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.Inventory.gui;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 /**
  *
  * @author jagrithnarayan
  */
-public class UpdateProductPanel {
+public class UpdateProductPanel extends JPanel
+{
+    private JTextField idField, priceField, quantityField;
+    private JButton updateButton;
+    private JLabel messageLabel;
+   
+    public UpdateProductPanel(ActionListener actionHandler)
+    {
+        setLayout(new GridLayout(5, 2, 10, 10));
+        
+        add(new JLabel("Product ID:"));
+        idField = new JTextField();
+        add(idField);
+        
+        add(new JLabel("New Price:"));
+        priceField = new JTextField();
+        add(priceField);
+        
+        add(new JLabel("New Quantity:"));
+        quantityField = new JTextField();
+        add(quantityField);
+        
+        updateButton = new JButton("Update Product");
+        updateButton.setActionCommand("UPDATE_PRODUCT");
+        updateButton.addActionListener(actionHandler);
+        add(updateButton);
+    }
     
+    public String getID() {
+        return idField.getText();
+    }
+    public String getPrice() {
+        return idField.getText();
+    }
+    public String getQuantity() {
+        return idField.getText();
+    }
+    public void showMessage(String msg)
+    {
+        messageLabel.setText(msg);
+    }
+    
+    public void clearFields()
+    {
+        idField.setText("");
+        priceField.setText("");
+        quantityField.setText("");
+    }
 }
--- src/com/Inventory/gui/ViewInventoryPanel.java
+++ src/com/Inventory/gui/ViewInventoryPanel.java
@@ -3,11 +3,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.Inventory.gui;
-
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
+import com.Inventory.model.Product;
 /**
  *
  * @author jagrithnarayan
  */
-public class ViewInventoryPanel {
+public class ViewInventoryPanel extends JPanel{
+    private JTable table;
+    private DefaultTableModel tableModel;
     
+    public ViewInventoryPanel() {
+        setLayout(new BorderLayout());
+        
+        // columns names for the table
+        String[] columnNames = {"ID", "Name", "Category", "Price", "Quantity"};
+        
+        //0 rows initially
+        tableModel = new DefaultTableModel(columnNames, 0);
+        
+        //table to show inventory 
+        table = new JTable(tableModel); 
+        
+        //mzake the table scrollable
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane, BorderLayout.CENTER);
+    }
+    
+    // now fill the table witjh product data
+    public void setProductList(List<Product> products)
+    {
+        tableModel.setRowCount(0);
+        for (Product p : products)
+        {
+            Object[] row = 
+            {
+                p.getId(),
+                p.getName(),
+                p.getCategory(),
+                String.format("%2f", p.getPrice()),
+                p.getQuantity()
+            };
+            
+            //add each product to new roll
+            tableModel.addRow(row);
+        }
+    }
 }
